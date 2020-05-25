8e9e93b69b33d34e9b4c0eeb46fe48298bde6c2a
dengliming <liming.d.pro@gmail.com>
2020-05-25 21:07:54 +0800

    Support for BACKENDSPATH <path> on AI.CONFIG

--- redisai/commands_test.go
+++ redisai/commands_test.go
@@ -1041,3 +1041,10 @@ func TestClient_ModelRun(t *testing.T) {
 		})
 	}
 }
+
+func TestCommand_SetBackendPath(t *testing.T) {
+	c := createTestClient()
+	ret, err := c.SetBackendPath("/usr/lib/redis/modules/backends/")
+	assert.Nil(t, err)
+	assert.Equal(t, "OK", ret)
+}
