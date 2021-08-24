365ed09e9a63925ba09e2068ff76278e0567749f
AvitalFineRedis <avital.fine@redislabs.com>
2021-08-24 12:22:33 +0300

    comments

--- redisai/implementations/AIScript.go
+++ redisai/implementations/AIScript.go
@@ -7,42 +7,52 @@ type AIScript struct {
 	entryPoints []string
 }
 
+//Device return the device type
 func (m *AIScript) Device() string {
 	return m.device
 }
 
+//SetDevice set the device type
 func (m *AIScript) SetDevice(device string) {
 	m.device = device
 }
 
+//Tag return the tag
 func (m *AIScript) Tag() string {
 	return m.tag
 }
 
+//SetTag set the tag
 func (m *AIScript) SetTag(tag string) {
 	m.tag = tag
 }
 
+//Source return the source
 func (m *AIScript) Source() string {
 	return m.source
 }
 
+//SetSource set the source
 func (m *AIScript) SetSource(source string) {
 	m.source = source
 }
 
+//EntryPoints return the entry points
 func (m *AIScript) EntryPoints() []string {
 	return m.entryPoints
 }
 
+//SetEntryPoints set the entry points
 func (m *AIScript) SetEntryPoints(entryPoinys []string) {
 	m.entryPoints = entryPoinys
 }
 
+//NewScript create a new AIScript with the given device 
 func NewScript(device string) *AIScript {
 	return &AIScript{device: device}
 }
 
+//NewEmptycript create empty AIScript 
 func NewEmptycript() *AIScript {
 	return &AIScript{}
 }
